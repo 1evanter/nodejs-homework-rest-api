58c4ad4a39a33977301b5635d5588f3bc4b53cb5
1evanter <rita.oleksenkoo@gmail.com>
2023-11-13 17:37:55 +0200

    add env

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const app = require("./app");
 const mongoose = require("mongoose");
+const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://levanter:319IJOOQjwHp68B3@cluster0.io0dw2f.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
@@ -10,7 +9,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(8000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
