153e10247ab0debb57409df7a78092ab4ecb112b
1evanter <rita.oleksenkoo@gmail.com>
2023-11-22 00:48:23 +0200

    update register controller
     add conflict error check

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,21 @@
 const { User } = require("../models/user");
 
-const { ctrlWrapper } = require("../helpers");
+const { ctrlWrapper, HttpError } = require("../helpers");
 
 const register = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email }).exec();
+
+  if (user !== null) {
+    throw HttpError(409, "Email in use");
+  }
+
   await User.create(req.body);
+
   res.status(201).json({
     user: {
-      email: "example@example.com",
+      email,
       subscription: "starter",
     },
   });
