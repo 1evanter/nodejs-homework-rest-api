ca6ab4208fcb682986dd022782613e8528a95fcc
1evanter <rita.oleksenkoo@gmail.com>
2023-11-23 00:48:36 +0200

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,13 +4,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  // const { page = 1, limit = 20 } = req.query;
-  // const skip = (page - 1) * limit;
-  // const result = await Contact.find({ owner }, { skip, limit }).populate(
-  //   "owner",
-  //   "email"
-  // );
-  const result = await Contact.find({ owner }).populate("owner", "email");
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
   res.status(200).json(result);
 };
 
